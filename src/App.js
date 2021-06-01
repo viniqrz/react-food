8a9ccf71af871fc69199b0eb9860b5c789c886a6
Vinicius <viniderp@gmail.com>
2021-06-01 12:14:20 -0300

    Test Data changes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Menu from './Components/Menu/Menu.js';
 
 const DUMMY_DISHES = [
   {id: Math.random()+'', name: 'Sushi', price: 22.99, desc: 'Finest Fish and Veggies'},
-  {id: Math.random()+'', name: 'Sushi', price: 22.99, desc: 'Finest Fish and Veggies'},
-  {id: Math.random()+'', name: 'Sushi', price: 22.99, desc: 'Finest Fish and Veggies'},
-  {id: Math.random()+'', name: 'Sushi', price: 22.99, desc: 'Finest Fish and Veggies'},
+  {id: Math.random()+'', name: 'Pizza', price: 14.99, desc: 'Mozzarella and Pepperoni'},
+  {id: Math.random()+'', name: 'Burger', price: 8.99, desc: 'Beef and Cheese'},
+  {id: Math.random()+'', name: 'Strawberry Smoothie', price: 6.99, desc: 'Strawberry and Milk'},
 ]
 
 const DUMMY_CART = []
